ec6d25ff90769ef368c2d145cd9ca106a6316926
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2025-01-10 11:05:08 -0700

    Make GRM on Alpine

--- gcta/1_make_gcta_grm.sh
+++ gcta/1_make_gcta_grm.sh
@@ -2,13 +2,13 @@
 
 # Create GCTA genetic relatedness matrix (GRM) from chip data in PLINK1.9.
 
-plink_input="${RKJCOLLAB}/Collabs/metaboxcan/raw/genetics/CG10k_NHW_hg19_Oct2017/CG10k_NHW_hg19_Oct2017.bed"
-gcta_dir="${RKJCOLLAB}/Collabs/metaboxcan/data/gcta"
-maf_filter=0.05
-grm_filter=0.05
+plink_input="$1"
+gcta_dir="$2"
+maf_filter=$3
+grm_filter=$4
 
 # Create matrix with filter MAF < 0.05
-plink_name=$(basename "${plink_input%.*}")
+plink_name=$(basename "$plink_input")
 gcta-1.94.1 --bfile "$plink_input" \
     --make-grm --maf $maf_filter \
     --out ${gcta_dir}/${plink_name}_maf${maf_filter}_grm
