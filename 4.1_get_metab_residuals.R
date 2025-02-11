71b6b5a81731092ff76c04aadc1e7aba2dcb118f
Sarah D. Slack <sarah.slack@cuanschutz.edu>
2025-02-11 08:23:00 -0700

    Add step to get adjusted metab residuals

---
+++ 4.1_get_metab_residuals.R
@@ -0,0 +1,117 @@
+# SDS 20250122
+
+# Modified from the script MLR.R in PredictDB tool on BioDataCatalyst.
+
+# Running with two different sets of covariates:
+  # 1. age and sex (most closely matches paper draft)
+  # 2. age, sex, race, smoking status, pack years, BMI, and FEV1pp (suggested
+    # by Katerina)
+
+# Subset to the 957 individuals that calculating accuracy in before running
+# model to get residuals here. To do this and keep all covars for option 2,
+# reduce to 953 individuals. Will run option 1 with all 957 and with 953 to
+# be comparable.
+
+# TO NOTE: select the above options and N individuals in script below.
+
+# R Script used for the Multiple Linear Regression
+# Script will loop through all the columns of the transposed gene expression
+# which correspond to each  gene and for each gene it runs linear regression
+# on the covariates. Then it sets the residuals to the new expression for that
+# gene.
+
+# Omics file format: A tab delimited file with .tab file extension containing
+# N + 1 rows and G + 1 columns, where N is the number of samples, and G is the
+# number of features (genes, methylation sites, chromatin accessibility windows,
+# etc.). The first row and column must contain sample IDs and feature IDs
+# respectively. Feature values should be normalized across samples and variance
+# stabilized.
+# Covariates file format: A tab deliminated file with N+1 rows and K+1 columns,
+# where N is the number of samples, and K is the desired covariates.
+# Output prefix format: File name prefix for output files.
+
+library(tidyverse)
+setwd(paste0(Sys.getenv("RKJCOLLAB"), "/Collabs/metaboxcan"))
+
+omic_file <- "raw/metab/COPDGene_P2_LT20miss_knnImp_NoOut_metabs_20211021.csv"
+covariates_file <- "raw/pheno/COPDGene_P1P2P3_Flat_SM_NS_Sep24.txt"
+
+# Also load list of individuals for accuracy to subset
+ids <- read_tsv("data/genetics/id_list_genetics_and_metab_split.txt",
+                col_names = c("fid", "iid"))
+
+# Read in data
+gene.exp <- read.csv(omic_file) %>%
+  column_to_rownames("sid")
+
+covar.data <- read_tsv(covariates_file) %>%
+  dplyr::select(sid, Age_P2, gender, race, smoking_status_P2, ATS_PackYears_P2,
+                BMI_P2, Pred_FEV1_P2) %>%
+  column_to_rownames("sid")
+# TO NOTE: this throws warnings, but doesn't affect the columns I need
+    # _P2 indicates phase 2 visit, which is visit with metabolomics
+  # Age = Age_P2, age at phase 2 visit?
+  # Sex = gender, 1= male, 2 = female
+  # Race = race
+  # Smoking status = smoking_status_P2
+  # Pack years = ATS_PackYears_P2
+  # BMI = BMI_P2
+  # FEV1pp = FEV1pp_post_P2 (FEV1 % pred, post-BD, confirmed with Katerina)
+
+# Subset individuals and check completeness of above variables
+covar.data.filt <- covar.data %>%
+  dplyr::filter(rownames(.) %in% ids$iid)
+gene.exp.filt <- gene.exp %>%
+  dplyr::filter(rownames(.) %in% ids$iid)
+
+sum(is.na(covar.data.filt))  # 13
+sum(is.na(covar.data.filt$Age_P2))  # 0
+sum(is.na(covar.data.filt$gender))  # 0
+sum(is.na(covar.data.filt$race))  # 0
+sum(is.na(covar.data.filt$smoking_status_P2))  # 3
+sum(is.na(covar.data.filt$ATS_PackYears_P2))  # 4
+sum(is.na(covar.data.filt$BMI_P2))  # 3, TODO: could calculate?
+sum(is.na(covar.data.filt$Pred_FEV1_P2))  # 3
+
+covar.check <- covar.data.filt %>%
+  dplyr::filter(is.na(ATS_PackYears_P2))
+# All NAs are for same 4 people
+
+# Run option 1
+# output_prefix <- "data/metab/COPDGene_P2_metabs_age_sex_covar"
+# covar.data.filt <- covar.data.filt %>%
+#   dplyr::select(Age_P2, gender)
+# sum(is.na(covar.data.filt))  # 0
+
+# Run option 1, subset to individuals for option 2
+# output_prefix <- "data/metab/COPDGene_P2_metabs_age_sex_covar_subset"
+# covar.data.filt <- na.omit(covar.data.filt)  # 953
+# covar.data.filt <- covar.data.filt %>%
+#   dplyr::select(Age_P2, gender)
+
+# Run option 2
+output_prefix <- "data/metab/COPDGene_P2_metabs_full_covar_subset"
+covar.data.filt <- na.omit(covar.data.filt)  # 953
+gene.exp.filt <- gene.exp.filt %>%
+  dplyr::filter(rownames(.) %in% rownames(covar.data.filt))
+
+# Order omics and covariates
+gene.exp.filt <- gene.exp.filt[rownames(covar.data.filt), ]
+identical(rownames(covar.data.filt), rownames(gene.exp.filt))
+
+# Make a copy of the gene.exp df and fill in with the residuals
+expression <- gene.exp.filt
+
+# Run MLR
+for (i in 1:length(colnames(gene.exp.filt))) {
+  fit <- lm(gene.exp.filt[,i] ~ as.matrix(covar.data.filt))
+  expression[,i] <- fit$residuals
+}
+
+# Reformat for accuracy calculation
+expression <- rownames_to_column(expression, "sid")
+
+# Write results
+write_tsv(expression, paste0(output_prefix, "_adj_resid.txt"))
+
+
--- 4.2_calc_prediction_accuracy.R
+++ 4.2_calc_prediction_accuracy.R
@@ -2,6 +2,19 @@
 
 # Calculate prediction accuracy.
 
+# Initially ran with metabolite input as provided. Now re-running with values
+# adjusted for different sets of covariates:
+
+# Running with two different sets of covariates:
+  # 1. age and sex (most closely matches paper draft)
+  # 2. age, sex, race, smoking status, pack years, BMI, and FEV1pp (suggested
+    # by Katerina)
+
+# Subset to the 957 individuals that calculating accuracy in before running
+# model to get residuals. To do this and keep all covars for option 2, reduce
+# to 953 individuals. Will run option 1 with all 957 and with 953 to be
+# comparable.
+
 # Setup ------------------------------------------------------------------------
 
 setwd(paste0(Sys.getenv("RKJCOLLAB"), "/Collabs/metaboxcan"))
@@ -11,10 +24,29 @@ library(data.table)
 library(conflicted)
 
 pred_gex <- read_delim(
-  "data/metsim_prediction/COPDGene_metsim_metaboxcan_predict.txt")
+  "data/metsim_prediction/filt/COPDGene_metsim_metaboxcan_predict.txt")
 
-metab <- read_csv(
-  "raw/metab/COPDGene_P2_LT20miss_knnImp_NoOut_metabs_20211021.csv")
+# Unadjusted
+# metab <- read_csv(
+#   "raw/metab/COPDGene_P2_LT20miss_knnImp_NoOut_metabs_20211021.csv")
+# output_prefix <- ""
+
+# Covar option 1
+# metab <- read_tsv(
+#   "data/metab/COPDGene_P2_metabs_age_sex_covar_adj_resid.txt")
+# output_prefix <- "age_sex_covar_adj_"
+
+# Covar option 1, subset to same individuals as option 2
+# metab <- read_tsv(
+#   "data/metab/COPDGene_P2_metabs_age_sex_covar_subset_adj_resid.txt")
+# output_prefix <- "age_sex_covar_subset_adj_"
+
+# Covar option 2
+metab <- read_tsv(
+  "data/metab/COPDGene_P2_metabs_full_covar_subset_adj_resid.txt")
+output_prefix <- "full_covar_subset_adj_"
+
+
 metab_info <- read_csv(
   "raw/metab/COPDGene_P2_MetaboliteInformation_20211021.csv")
 
@@ -53,17 +85,17 @@ pred_gex_mod$pop = "pred.expr"
 metab_ids <- intersect(colnames(metab_mod), colnames(pred_gex_mod))  # 790
 n_distinct(metab_ids)
   # 789 without pop
-indv_ids <- intersect(metab_mod$sid, pred_gex_mod$ID)  # 957
+indv_ids <- intersect(metab_mod$sid, pred_gex_mod$ID)  # 957/953
 n_distinct(indv_ids)
 
 metab_mod_filt <- metab_mod %>%
   dplyr::filter(sid %in% indv_ids) %>%
-  dplyr::select(sid, all_of(metab_ids)) %>% # 957 by 791 (with pop and id col)
+  dplyr::select(sid, all_of(metab_ids)) %>% # 957/953 by 791 (with pop and id col)
   dplyr::rename(ID = sid)
 
 pred_gex_mod_filt <- pred_gex_mod %>%
   dplyr::filter(ID %in% indv_ids) %>%
-  dplyr::select(ID, all_of(metab_ids))  # 957 by 791
+  dplyr::select(ID, all_of(metab_ids))  # 957/953 by 791
 
 # Merge
 merge <- rbind(metab_mod_filt, pred_gex_mod_filt) %>%
@@ -77,6 +109,8 @@ merge <- rbind(metab_mod_filt, pred_gex_mod_filt) %>%
   # Checking the Predixcan paper by Gamazon (PMID 6258848). From the methods:
   # "To assess performance, we used the square of the Pearson correlation, R2,
   # between predicted and observed expression levels."
+  # UPDATE: Since now analyzing residuals, thought Spearman would no longer have
+  # issues with ties, but still does.
 
 # Function to get a data frame of each metab, R2, and correlation
 # df <- merge
@@ -135,27 +169,24 @@ sum(is.na(merge_mod$gender))  # 0 NA
 
 merge_mod_m <- merge_mod %>%
   dplyr::filter(gender == 1)
-n_distinct(merge_mod_m$ID)  # 489 males
+n_distinct(merge_mod_m$ID)  # 489/487 males
 merge_mod_f <- merge_mod %>%
   dplyr::filter(gender == 2)
-n_distinct(merge_mod_f$ID)  # 468 females
+n_distinct(merge_mod_f$ID)  # 468/466 females
 
 result_m <- get_pred_vs_obs(merge_mod_m, metabs)
 result_f <- get_pred_vs_obs(merge_mod_f, metabs)
 
 # Write out --------------------------------------------------------------------
 
-write_tsv(
-  merge, "data/metsim_prediction/pred_v_obs_metsim.txt")
+write_tsv(merge, paste0(
+  "data/metsim_prediction/filt/COPDGene_", output_prefix, "pred_v_obs_metsim.txt"))
 
-write_tsv(
-  result,
-  "data/metsim_prediction/COPDGene_metsim_prediction_performance.txt")
+write_tsv(result, paste0(
+  "data/metsim_prediction/filt/COPDGene_", output_prefix, "metsim_prediction_performance.txt"))
 
-write_tsv(
-  result_m,
-  "data/metsim_prediction/COPDGene_metsim_prediction_performance_males.txt")
+write_tsv(result_m, paste0(
+  "data/metsim_prediction/filt/COPDGene_", output_prefix, "metsim_prediction_performance_males.txt"))
 
-write_tsv(
-  result_f,
-  "data/metsim_prediction/COPDGene_metsim_prediction_performance_females.txt")
+write_tsv(result_f, paste0(
+  "data/metsim_prediction/filt/COPDGene_", output_prefix, "metsim_prediction_performance_females.txt"))
